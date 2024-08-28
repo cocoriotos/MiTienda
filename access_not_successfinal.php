b160c9af2bd1e553260aa4fef2bdfaca8481c67b
Julian GOnzalez <cocoriotos@hotmail.com>
2024-08-28 16:48:37 -0500

    MissedFilesAssets

---
+++ access_not_successfinal.php
@@ -0,0 +1,9 @@
+<!--  Developed by julián González Bucheli -->
+<html lang="us"> <!-- Page language-->
+		  <script>
+			alert("ALERTA: Su nombre de usuario es incorrecto, por favor trate nuevamente o seleccinoe la opción de requerir acceso");
+		  </script>
+	<?php
+	include("closecon.php");
+	?>
+</html>
--- access_successfinaladm.php
+++ access_successfinaladm.php
@@ -5,7 +5,7 @@
 //Defining local procedure variables and assigning POST form values into
 	$username=$_POST['username'];
 	$password=$_POST['password1'];
-
+	$tiendaname=$_POST['tiendaname'];
 
 	
 	if($_POST)
@@ -13,48 +13,45 @@
   $db_host="localhost:3307";
   $db_user="root";
   $db_pass="P@ssw0rd123456789";
-  $db_name="appsdb";
-  //$username=$_POST['username'];
+  $db_name="tiendas";
   $conn=mysqli_connect($db_host,$db_user,$db_pass);/*$conn store the parameter information */
   if(mysqli_connect_errno()) /*if mysql connection is not success then will generate error message*/
 			{	
-			/*alert("ALERT: DB not connected");*/
 			include("No_DB_Connectionfinal.php");
-			include ("mitiendaadmlogin.php");
+			include("mitiendaadmlogin.php");
 			exit();/*Break php file and next lines*/
 			}
-  mysqli_select_db($conn,$db_name) or die ("<center>There is not Database available</center>");/*database is not available*/			
+  mysqli_select_db($conn,$db_name) or die ("<center>No hay conexión disponible</center>");/*database is not available*/			
 
   if ($conn==true)
 		{
 
 		}
 
-		$insert="update accesslistmitiendaadm SET visits=visits+1 where (username='$username' and password='$password')"; /*username Vistit counter increas in 1*/
+		$insert="update tiendasaccesslist SET visits=visits+1 where (username='$username' and password='$password' and tiendaname='$tiendaname')"; /*username Vistit counter increas in 1*/
         $result=mysqli_query($conn, $insert);
-		$query1="select * from accesslistmitiendaadm where username='$username' and active='1' and role='user' and password='$password'"; /*if username is user role*/
+		$query1="select * from tiendasaccesslist where username='$username' and active='1' and role='user' and password='$password' and tiendaname='$tiendaname'"; /*if username is user role*/
 		$result1=mysqli_query($conn, $query1);
-		$query2="select * from accesslistmitiendaadm where username='$username' and active='1' and role='admin' and password='$password'"; /*if username is admin role*/
+		$query2="select * from tiendasaccesslist where username='$username' and active='1' and role='admin' and password='$password' and tiendaname='$tiendaname'"; /*if username is admin role*/
 		$result2=mysqli_query($conn, $query2);
 
 
 		
 		if(mysqli_num_rows($result1)==true)
 			{	
-			include("livraria_admin.php");
+			##include("livraria_admin.php");
 			exit();	
 			}
 		else 
 		{
 			if(mysqli_num_rows($result2)==true)
 		    	{	
-			     include ("livraria_admin.php");
-			     exit();	
+				 include("TiendaAdminModule.php");
+				 exit();	
 			   } else{
-			include ("access_not_successfinal.php");
+			include("access_not_successfinal.php");
 			}
 		}
-		
  }
  
  /*mysqli_close($conn);*/
